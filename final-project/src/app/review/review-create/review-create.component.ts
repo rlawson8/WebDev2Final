b0b5ab02ac432993e0406b0eae2229ecb391f3ca
Reid Lawson <rldc6@umsystem.edu>
2023-04-16 18:24:57 -0500

    Added base components for the form and footer

---
+++ final-project/src/app/review/review-create/review-create.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector:'review-create',
+  templateUrl: './review-create.component.html',
+  styleUrls:['../styles.css']
+})
+export class ReviewCreateComponent {
+
+  onAddReview(form: NgForm){
+
+  }
+}
