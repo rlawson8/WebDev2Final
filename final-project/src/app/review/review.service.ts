20a7d546e92605ff029cc53a8a116e92bdb666fe
Reid Lawson <rldc6@umsystem.edu>
2023-04-17 17:40:13 -0500

    Added service and model file and changed some css

---
+++ final-project/src/app/review/review.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { Review } from "./review.model";
+
+@Injectable({providedIn: 'root'})
+
+export class ReviewService {
+  getReviews(){
+
+  }
+
+  addReviews(title: string, body: string, rating: number, name:string){
+
+  }
+}
