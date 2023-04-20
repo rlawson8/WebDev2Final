fee058530581f52a3485e1bfbfa35cb4ca6b65f1
jckcob <112060475+JNTGYV@users.noreply.github.com>
2023-04-20 17:46:15 -0500

    Fixed header, Added/Organized imports

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { ReviewCreateComponent } from './review/review-create/review-create.component';
+import { MatMenuModule } from '@angular/material/menu';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FooterComponent } from './footer/footer.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppComponent } from './app.component';
+import { ReviewCreateComponent } from './review/review-create/review-create.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header-component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReviewCreateComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,9 +32,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatButtonModule,
     MatToolbarModule,
-    NgbModule
+    NgbModule,
+    MatMenuModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
